e9c5d065269d22a54ae7de385f902026e27b94ac
RK905 <mr.rayenk@gmail.com>
2015-07-10 12:55:18 -0400

    Initial Commit

---
+++ MutiCell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MutiCell
+//
+//  Created by Rayen Kamta on 7/10/15.
+//  Copyright (c) 2015 Geeksdobyte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
